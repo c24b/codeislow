71d7edb90e0181412ef7255aee5bf5ac6ca8d075
c24b Constance de Quatrebarbes <4barbes@gmail.com>
2022-11-10 16:37:31 +0100

    [CODE_REF] TESTING source code + filter FIX ValueError

---
+++ src/tests/context.py
@@ -0,0 +1,12 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import parsing
+import code_references
+import matching
+import request_api
+import codeislow
+import app
+
+

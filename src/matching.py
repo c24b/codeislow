9352615fbc92c414312c085934f377073138238a
c24b Constance de Quatrebarbes <4barbes@gmail.com>
2022-11-11 03:46:38 +0100

    [FEAT] TEST reversed pattern

---
+++ src/matching.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python3
+# filename: matching.py
+"""
+The matching module
+
+Ce module permet la detection des articles du code de droit français
+
+"""
+import logging
+import re
+
+from code_references import filter_code_regex, filter_code_reference, CODE_REGEX
+
+ARTICLE_REGEX = r"(?P<art>(Articles?|Art\.))"
+
+ARTICLE_REF = r"\d+"
+ARTICLE_NUM = r"(?P<ref>.*?\d{1,4}(.?-\d{1,4}.?)?)"
+
+def switch_pattern(selected_codes=None, pattern="article_code"):
+    """
+    Build pattern recognition using pattern short code switch
+
+    Arguments
+    ---------
+    selected_codes: array
+        a list of short codes to select. Default to None
+    pattern: str
+        a string article_code or code_article. Default to article_code
+    Returns
+    ---------
+    regex_pattern: str
+        a compiled regex pattern
+    Raise
+    --------
+    ValueError:
+        pattern name is wrong
+    """
+
+    code_regex = filter_code_regex(None)
+    
+    if pattern not in ["article_code", "code_article"]:
+        raise ValueError(
+            "Wrong pattern name: choose between 'article_code' or 'code_article'"
+        )
+    if pattern == "article_code":
+        return re.compile(f"{ARTICLE_REGEX}(?P<ref>.*?){code_regex}", flags=re.I)
+    else:
+        return re.compile(f"{code_regex}", flags=re.I), re.compile(f"({ARTICLE_REGEX})"+r"?.*?(?P<ref>.*\d*)", flags=re.I)
+        
+def get_code_refs_reversed_pattern(full_text, pattern_regex, selected_codes):
+    code_regex, article_regex = pattern_regex
+    # code_regex = filter_code_regex(None)
+    # code_regex = re.compile(f"{code_regex}", flags=re.I)
+    # article_regex = re.compile(f"{ARTICLE_REGEX}.*?(?P<ref>.*\d*)", flags=re.I)
+    selected_codes = filter_code_reference(selected_codes)   
+    code_list = []
+    code_name_list = []
+    for i, match in enumerate(re.finditer(code_regex, full_text)):
+        needle = match.groupdict()
+        assert needle is not None, needle
+        for key, value in needle.items():
+            if value is not None and key in list(selected_codes):
+                code_list.append(key)
+                code_name_list.append(value)
+    
+    text_by_codes = []
+    for i,item in enumerate(code_name_list):
+        chunk0, chunk1 = full_text.split(item,1)
+        if i != 0:
+            text_by_codes.append(chunk0)
+        full_text = chunk1
+        if i+1 == len(code_name_list):
+            text_by_codes.append(chunk1) 
+    for code, chunk_text in zip(code_list,text_by_codes): 
+        print(code, chunk_text)
+        for match in re.finditer(article_regex, chunk_text):
+            if code in list(selected_codes):
+                match = match.group("ref").strip()
+                if match != '':
+                    yield code, match
+    
+def get_code_refs_classical_pattern(full_text:str, article_pattern:str, selected_codes:list):
+    
+    for i, match in enumerate(re.finditer(article_pattern, full_text)):
+        needle = match.groupdict()
+        qualified_needle = {
+            key: value for key, value in needle.items() if value is not None
+        }
+        msg = f"#{i+1}\t{qualified_needle}"
+        print(msg)
+        # logging.debug(msg)
+        # get the code shortname based on regex group name <code>
+        code = [k for k in qualified_needle.keys() if k not in ["ref", "art"]][0]
+        if code in selected_codes:
+            yield (code, match.group("ref").strip())
+
+def get_code_refs(full_text, pattern_format, selected_codes):
+    #Force to detect every article of every code
+    article_pattern = switch_pattern(None, pattern_format)
+    if pattern_format == "article_code":
+        return get_code_refs_classical_pattern(full_text, article_pattern, selected_codes)
+    else:
+        return get_code_refs_reversed_pattern(full_text, article_pattern, selected_codes)
+
+def normalize_references(ref):
+    # split multiple articles of a same code
+    refs = [
+        n
+        for n in re.split(r"(\set\s|,\s|\sdu)", ref)
+        if n not in [" et ", ", ", " du", " ", ""]
+    ]
+    # normalize articles to remove dots, spaces, caret and 'alinea'
+    # with keeping only accepted caracters for article
+    # i.e. numbers, (L|A|R|D) and caret
+    # exemple: 1224 du => Non 298 al 32 => Non R-288 => oui A-24-14=> oui
+    refs = [
+        "-".join(
+            [
+                r
+                for r in re.split(r"\s|\.|-", ref)
+                if r not in [" ", "", "al", "alinea", "alinéa"]
+            ]
+        )
+        for ref in refs
+    ]
+    
+    normalized_refs = []
+    for ref in refs:
+        ref = "".join([r for r in ref if r.isdigit() or r in ["L", "A", "R", "D", "-"]])
+        ref = re.sub(r'-{2,}', "", ref)
+        #remove first caret -2323 => 2323
+        if ref.endswith("-"):
+            ref = ref[:-1]
+        # remove caret separating article nb between first letter
+        # exemple: L-248-1 = > L248-1
+        special_ref = ref.split("-", 1)
+        if special_ref[0] in ["L", "A", "R", "D"]:
+            normalized_refs.append("".join(special_ref))
+        
+        else:
+            if ref not in ["", " "]:
+                normalized_refs.append(ref)
+    return normalized_refs
+
+def get_matching_results_dict(
+    full_text, selected_short_codes=[], pattern_format="article_code"
+):
+    """
+    Une fonction qui renvoie un dictionnaire de resultats:
+    trié par code (version abbréviée) avec la liste des articles
+    détectés lui appartenant.
+
+    Arguments
+    ----------
+    full_text: str
+        a string of the full document normalized
+    pattern_format: str
+        a string representing the pattern format article_code or code_article. Defaut to article_code
+
+    Returns
+    ----------
+    code_found: dict
+        a dict compose of short version of code as key and list of the detected articles references  as values {code: [art_ref, art_ref2, ... ]}
+    """
+    selected_codes = filter_code_reference(selected_short_codes)
+    
+    code_found = {}
+    # normalisation
+    full_text = re.sub(r"\s{2,}|\r{1,}|\n{1,}|\t{1,}|\xa0{1,}", " ", " ".join(full_text))
+    for code, ref in get_code_refs(full_text, pattern_format,selected_codes):
+        if code not in code_found:
+            # append article references
+            code_found[code] = normalize_references(ref)
+        else:
+            # append article references to existing list
+            code_found[code].extend(normalize_references(ref))
+    return code_found
+
+
+def get_matching_result_item(
+    full_text, selected_shortcodes=[], pattern_format="article_code"
+):
+    """ "
+    Renvoie les références des articles détectés dans le texte
+
+    Arguments
+    -----------
+    full_text: str
+        a string of the full document normalized
+    selected_shortcodes: array
+        a list of selected codes in short format for filtering article detection. Default is an empty list (which stands for no filter)
+    pattern_format: str
+    a string representing the pattern format article_code or code_article. Defaut to article_code
+
+    Yields
+    --------
+    code_short_name:str
+
+    article_number:str
+    """
+    selected_codes = filter_code_reference(selected_shortcodes)
+    
+    full_text = re.sub(r"\s{2,}|\r{1,}|\n{1,}|\t{1,}|\xa0{1,}", " ", " ".join(full_text))
+    for code, ref in get_code_refs(full_text, pattern_format,selected_codes):
+        yield (code, normalize_references(ref))
+    
---
+++ src/tests/test_003_matching.py
@@ -0,0 +1,247 @@
+import os
+import pytest
+import re
+from .context import matching, parsing, code_references
+from parsing import parse_doc
+from code_references import CODE_REGEX, filter_code_regex
+from matching import get_matching_results_dict, get_matching_result_item, switch_pattern, get_code_refs
+from .test_001_parsing import restore_test_file, archive_test_file
+
+
+class TestMatching:
+    def test_switch_pattern_wrong_pattern(self):
+        with pytest.raises(ValueError) as e:
+            switch_pattern(None, "code_code")
+            assert e.info_msg == "", e
+
+    def test_switch_pattern_classical_pattern(self):        
+        pattern = switch_pattern(None, "article_code")
+        assert repr(pattern).startswith("re.compile("), repr(pattern)
+
+    def test_switch_pattern_reverse_pattern(self):        
+        pattern_code, pattern_article = switch_pattern(None, "code_article")
+        assert repr(pattern_code).startswith("re.compile("), repr(pattern_code)
+        assert repr(pattern_article).startswith("re.compile("), repr(pattern_article)
+    
+    @pytest.mark.parametrize("input_expected", [
+        ("Comme il est mentionné dans l'article 238-4 alinéa 2 du code de la consommation .", ["Code de la consommation","238-4"]),
+        ("Comme il est mentionné dans l'art.238-4 alinéa 2 du C.conso.", ["Code de la consommation","238-4"]),
+        ("Art. 2-3-4 du Code de l'Environnement", ["Code de l'environnement", "2-3-4"]),
+        ("Art. R2-3-4 du CE.", ["Code de l'environnement", "R2-3-4"]),
+        ("\n-\n article L.278 du cjaaa", ["Code de justice administrative", "L278"]),
+        ("Art. L. 385-2, R. 343-4 et A421-13 du C. assur. ", [["Code des assurances", "L385-2"], ["Code des assurances","R343-4"], ["Code des assurances", "A421-13"]]) 
+    ])
+    def test_matching_result_item(self, input_expected):
+        input, expected = input_expected
+        for item in get_matching_result_item(input):
+            assert item == expected, item
+    @pytest.mark.parametrize("input_expected", [
+        ("Comme il est mentionné dans le code de la consommation article 238-4 alinéa 2.", ["Code de la consommation","238-4"]),
+        ("Comme il est mentionné dans  le C.conso. art.238-4 alinéa 2  qui blablabla", ["Code de la consommation","238-4"]),
+        ("Code de l'Environnement Art. 2-3-4", ["Code de l'environnement", "2-3-4"]),
+        ("CE. Art. R2-3-4", ["Code de l'environnement", "R2-3-4"]),
+        ("\n-\n  cjaaa article L.278 ", ["Code de justice administrative", "L278"]),
+        ("C. assur. Art. L. 385-2, R. 343-4 et A421-13", [["Code des assurances", "L385-2"], ["Code des assurances","R343-4"], ["Code des assurances", "A421-13"]])
+    ])
+    def test_matching_result_item_reversed_pattern(self, input_expected):
+        input, expected = input_expected
+        for item in get_matching_result_item(input, [], "code_article"):
+            assert item == expected, item
+
+    def test_matching_result_dict_codes_no_filter_pattern_article_code(self):
+        # NO CPCE ref dans le doc
+        code_reference_test = CODE_REGEX
+        del code_reference_test["CPCE"]
+
+        file_paths = ["newtest.doc", "newtest.docx", "newtest.pdf"]
+        for file_path in file_paths:
+            
+            tmp_abspath = archive_test_file(file_path)
+            abspath = os.path.join(
+                os.path.dirname(os.path.realpath(__file__)), file_path
+            )
+            full_text = parse_doc(abspath)
+            restored_abspath = restore_test_file(file_path)
+            results_dict = get_matching_results_dict(full_text, None, "article_code")
+
+            # del code_reference_test["CPCE"]
+            code_list = list(results_dict.keys())
+            assert len(code_list) == len(code_reference_test), set(
+                code_reference_test
+            ) - set(code_list)
+            assert sorted(code_list) == sorted(code_reference_test), set(
+                code_reference_test
+            ) - set(code_list)
+            articles_detected = [
+                item for sublist in results_dict.values() for item in sublist
+            ]
+            assert len(articles_detected) == 37, len(articles_detected)
+            assert results_dict["CCIV"] == [
+                "1120",
+                "2288",
+                "1240-1",
+                "1140",
+                "1",
+                "349",
+                "39999",
+                "3-12",
+                "12-4-6",
+                "14",
+                "15",
+                "27",
+            ], results_dict["CCIV"]
+            assert results_dict["CPRCIV"] == ["1038", "1289-2"], results_dict["CPRCIV"]
+            assert results_dict["CASSUR"] == [
+                "L385-2",
+                "R343-4",
+                "A421-13",
+            ], results_dict["CASSUR"]
+            assert results_dict["CCOM"] == ["L611-2"], results_dict["CCOM"]
+            assert results_dict["CTRAV"] == ["L1111-1"], results_dict["CTRAV"]
+            assert results_dict["CPI"] == ["L112-1", "L331-4"], results_dict["CPI"]
+            assert results_dict["CPEN"] == ["131-4", "225-7-1"], results_dict["CPEN"]
+            assert results_dict["CPP"] == ["694-4-1", "R57-6-1"], results_dict["CPP"]
+            assert results_dict["CCONSO"] == ["L121-14", "R742-52"], results_dict[
+                "CCONSO"
+            ]
+            assert results_dict["CSI"] == ["L622-7", "R314-7"], results_dict["CSI"]
+            assert results_dict["CSS"] == ["L173-8"], results_dict["CSS"]
+            assert results_dict["CSP"] == ["L1110-1"], results_dict["CSP"]
+            assert results_dict["CENV"] == ["L124-1"], ("CENV", results_dict["CENV"])
+            assert results_dict["CJA"] == ["L121-2"], ("CJA", results_dict["CJA"])
+            assert results_dict["CGCT"] == ["L1424-71", "L1"], (
+                "CGCT",
+                results_dict["CGCT"],
+            )
+            assert results_dict["CESEDA"] == ["L753-1", "12"], (
+                "CESEDA",
+                results_dict["CESEDA"],
+            )
+
+    def test_matching_result_dict_codes_unique_filter_pattern_article_code(self):
+        selected_codes = ["CASSUR"]
+        file_paths = ["newtest.doc", "newtest.docx", "newtest.pdf"]
+        for file_path in file_paths:
+            archive_test_file(file_path)
+            abspath = os.path.join(
+                os.path.dirname(os.path.realpath(__file__)), file_path
+            )
+            full_text = parse_doc(abspath)
+            restore_test_file(file_path)
+            results_dict = get_matching_results_dict(
+                full_text, selected_codes, "article_code"
+            )
+            code_list = list(results_dict.keys())
+            assert len(code_list) == len(selected_codes), len(code_list)
+            assert sorted(code_list) == ["CASSUR"], sorted(code_list)
+            articles_detected = [
+            item for sublist in results_dict.values() for item in sublist
+            ]
+            assert len(articles_detected) == 3, len(articles_detected)
+            assert results_dict["CASSUR"] == [
+                "L385-2",
+                "R343-4",
+                "A421-13",
+            ], results_dict["CASSUR"]
+
+    def test_matching_result_dict_codes_multiple_filter_pattern_article_code(self):
+        selected_codes = ["CASSUR", "CENV", "CSI", "CCIV"]
+        file_paths = ["newtest.doc", "newtest.docx", "newtest.pdf"]
+        for file_path in file_paths:
+            abspath = os.path.join(
+                os.path.dirname(os.path.realpath(__file__)), file_path
+            )
+            archive_test_file(file_path)
+            full_text = parse_doc(abspath)
+            restore_test_file(file_path)
+            results_dict = get_matching_results_dict(
+                full_text, selected_codes, "article_code"
+            )
+            code_list = list(results_dict.keys())
+            assert len(code_list) == len(selected_codes), len(code_list)
+            assert sorted(code_list) == [
+                "CASSUR",
+                "CCIV",
+                "CENV",
+                "CSI",
+            ], sorted(code_list)
+            
+            articles_detected = [
+            item for sublist in results_dict.values() for item in sublist
+            ]
+            assert len(articles_detected) == 18, len(articles_detected)
+            assert results_dict["CCIV"] == [
+                "1120",
+                "2288",
+                "1240-1",
+                "1140",
+                "1",
+                "349",
+                "39999",
+                "3-12",
+                "12-4-6",
+                "14",
+                "15",
+                "27",
+            ], results_dict["CCIV"]
+            assert results_dict["CASSUR"] == ["L385-2", "R343-4", "A421-13"], results_dict[
+                "CASSUR"
+            ]
+            assert results_dict["CSI"] == ["L622-7", "R314-7"], results_dict["CSI"]
+            assert results_dict["CENV"] == ["L124-1"], ("CENV", results_dict["CENV"])
+    
+    def test_get_code_refs_reversed_pattern(self):
+        full_text = "CSI Article LL22-7 et R314-7  Code de l'environnement L124-1 CJA L214-4"
+        
+        results = [n for n in (get_code_refs(full_text, "code_article", selected_codes=None))]
+        assert len(results) == 3, results
+
+    def test_get_code_refs_reversed_pattern_001(self):
+        full_text = "Comme on peut le voir dans CSI Article LL22-7 et R314-7  Code de l'environnement L124-1 CJA L214-4"
+        
+        results = [n for n in (get_code_refs(full_text, "code_article", selected_codes=None))]
+        assert len(results) == 3, results
+    def test_matching_result_dict_codes_no_filter_reversed_pattern_paragraphs(self):
+        # NO CPCE ref dans le doc
+        code_reference_test = CODE_REGEX
+        # del code_reference_test["CPCE"]
+
+        
+        chunk = "C’est la solution posée dans le Code civil article 1120"
+        for item in get_matching_result_item(chunk):
+            assert item  == ("Code civil", "1120"), item
+        for code, item in get_matching_results_dict(chunk):
+            assert code == "Code civil"
+            assert item == "1120"
+
+        multi_chunk = "C’est la solution posée dans le Code civil article 1120 et de C. civ. art. 2288"
+        for i, item in enumerate(get_matching_result_item(multi_chunk)):
+            if i == 0:
+                assert item  == ("Code civil", "1120"), item
+            else:
+                assert item  == ("Code civil", "2288"), item
+    
+    def test_matching_result_dict_codes_no_filter_pattern_code_article(self):
+        
+        code_reference_test_002 = CODE_REGEX
+        file_paths = ["testnew.odt", "testnew.pdf", "testnew.docx"]
+        for file_path in file_paths:
+            
+            tmp_abspath = archive_test_file(file_path)
+            abspath = os.path.join(
+                os.path.dirname(os.path.realpath(__file__)), file_path
+            )
+            full_text = parse_doc(abspath)
+            restored_abspath = restore_test_file(file_path)
+            results_dict = get_matching_results_dict(
+                full_text, None, "article_code"
+            )
+            assert sorted(list(results_dict.keys())) == sorted(code_reference_test_002), sorted(code_reference_test_002)
+            assert len(results_dict) == 16, len(results_dict)
+            assert len(list(results_dict.values())) == 16, len(list(results_dict.values()))
+            assert results_dict["CCIV"] == ['1120', '2288', '1240-1', '1140', '1', '349', '39999'], results_dict["CCIV"]
+            # assert results_dict["CASSUR"] == ["L385-2", "R343-4", "A421-13"], results_dict[
+            #     "CASSUR"
+            # ]
+            # assert results_dict["CSI"] == ["L622-7", "R314-7"], results_dict["CSI"]
+            # assert results_dict["CENV"] == ["L124-1"], ("CENV", results_dict["CENV"])

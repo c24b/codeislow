3f641053599d8fd57db6834dc5312924a0de14f5
c24b Constance de Quatrebarbes <4barbes@gmail.com>
2022-11-12 21:52:36 +0100

    [TEST] dirty fix to load source_code within test

--- src/tests/context.py
+++ src/tests/context.py
@@ -1,10 +1,12 @@
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import parsing
 import code_references
 import matching
+import check_validity
 import request_api
 import codeislow
 import app
